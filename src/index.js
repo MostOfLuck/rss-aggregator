07882baaf708a6f3fcc681625c1c315d9854ad06
Sam <burtman.samuel26@gmail.com>
2024-01-16 21:36:33 +0200

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
-import { initRssFormView } from './view';
-import { fetchRSS, parseRSS } from './rssFetcher';
-import { displayRSS } from './rssRenderer';
-import './i18n';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import init from './init.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const existingUrls = [];
-
-  initRssFormView(existingUrls);
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const rssForm = document.getElementById('rssForm');
-  const rssInput = document.getElementById('rssInput');
-  const feedContainer = document.getElementById('feedContainer'); // Контейнер для отображения фидов
-
-  rssForm.addEventListener('submit', event => {
-    event.preventDefault();
-    const url = rssInput.value;
-    fetchRSS(url)
-      .then(parseRSS)
-      .then(feed => {
-        displayRSS(feed, 'feedContainer');
-        rssInput.value = '';
-      })
-      .catch(error => {
-        console.error('Ошибка при загрузке RSS:', error);
-      });
-  });
-});
+init();
