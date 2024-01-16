c3d72423ae68d69502aa5e12c6cd6438a8d93dff
Sam <burtman.samuel26@gmail.com>
2024-01-16 21:36:46 +0200

    updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,56 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+import path, { dirname } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
 import { fileURLToPath } from 'url';
-import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirName = dirname(filename);
 
-export default {
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
   entry: './src/index.js',
-
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(dirName, 'dist'),
   },
-
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: ['file-loader']
-      }
-    ]
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
   },
+};
 
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ]}
+export default () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
