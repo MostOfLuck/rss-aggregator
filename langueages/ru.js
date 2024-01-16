f6e5b7476b558e2377e6f31c72e7c4dff7436823
Sam <burtman.samuel26@gmail.com>
2024-01-16 21:39:27 +0200

    languages added: rus, eng

---
+++ langueages/ru.js
@@ -0,0 +1,27 @@
+export default {
+  translation: {
+    successed: {
+      rssLoaded: 'RSS успешно загружен',
+    },
+    errors: {
+      urlInvalid: 'Ссылка должна быть валидным URL',
+      rssInvalid: 'Ресурс не содержит валидный RSS',
+      rssDuplicated: 'RSS уже существует',
+      networkError: 'Ошибка сети',
+    },
+    elements: {
+      title: 'RSS агрегатор',
+      subTitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      rssLink: 'Ссылка RSS',
+      mainButton: 'Добавить',
+      exampleUrl: 'Пример: https://ru.hexlet.io/lessons.rss',
+      feeds: 'Фиды',
+      posts: 'Посты',
+      postButton: 'Просмотр',
+    },
+    modal: {
+      linkButton: 'Читать полностью',
+      closeButton: 'Закрыть',
+    },
+  },
+};
