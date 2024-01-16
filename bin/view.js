32c7fa6f762632f111dfe228be1d9058078eb497
Sam <burtman.samuel26@gmail.com>
2024-01-16 21:34:41 +0200

    updated bin/view.js

--- bin/view.js
+++ bin/view.js
@@ -1,26 +1,46 @@
-import { validateURL } from './urlValidation';
-import i18n from './i18n';
+import onChange from 'on-change';
+import processStateHandler from './stateHandler.js';
+import errorsHandler from './errorsHandler.js';
+import renderFeeds from './rssFetcher.js';
+import renderPosts from './postsRender.js';
+import clickedPostsHandler from './postHandler.js';
+import renderModal from './rssRenderer.js';
 
-export const initRssFormView = (existingUrls) => {
-  const form = document.getElementById('rssForm');
-  const urlInput = document.getElementById('rssInput');
-  const validationError = document.getElementById('validationError');
+const render = (elements, state, i18n) => (path, value) => {
+  switch (path) {
+    case 'form.processState':
+      processStateHandler(elements, value, state, i18n);
+      break;
 
-  form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const url = urlInput.value;
+    case 'form.valid':
+      break;
 
-      validateURL(url, existingUrls)
-          .then((result) => {
-              if (result.valid) {
-                  validationError.textContent = '';
-                  urlInput.classList.remove('is-invalid');
-                  // Дополнительные действия для валидного URL
-              }
-          })
-          .catch((error) => {
-              validationError.textContent = i18n.t(error.message);
-              urlInput.classList.add('is-invalid');
-          });
-  });
+    case 'form.error':
+      errorsHandler(elements, value, i18n);
+      break;
+
+    case 'form.urls':
+      break;
+
+    case 'feeds':
+      renderFeeds(elements, state, i18n);
+      break;
+
+    case 'posts':
+      renderPosts(elements, state, i18n);
+      break;
+
+    case 'modal.clickedPost':
+      clickedPostsHandler(state);
+      break;
+
+    case 'modal.clickedPostId':
+      renderModal(elements, state, i18n);
+      break;
+
+    default:
+      throw new Error(`unknown path ${path}`);
+  }
 };
+
+export default (state, elements, language) => onChange(state, render(elements, state, language));
