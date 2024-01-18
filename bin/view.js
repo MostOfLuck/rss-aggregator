924262e73bbe130ffabeaba6dda0e402e4fa94e3
Sam <burtman.samuel26@gmail.com>
2024-01-18 20:26:18 +0200

    update bin/view.js

--- bin/view.js
+++ bin/view.js
@@ -1,10 +1,10 @@
 import onChange from 'on-change';
 import processStateHandler from './stateHandler.js';
 import errorsHandler from './errorsHandler.js';
-import renderFeeds from './renderModal.js';
+import renderFeeds from './renderFeed.js';
 import renderPosts from './postsRender.js';
 import clickedPostsHandler from './postHandler.js';
-import renderModal from './renderFeed.js';
+import renderModal from './renderModal.js';
 
 const render = (elements, state, i18n) => (path, value) => {
   switch (path) {
