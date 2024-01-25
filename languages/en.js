ef639705aa5fa5bbfbd4b4d216d0f84153609573
Sam <burtman.samuel26@gmail.com>
2024-01-25 15:40:17 +0200

    update languages/

---
+++ languages/en.js
@@ -0,0 +1,27 @@
+export default {
+  translation: {
+    successed: {
+      rssLoaded: 'RSS has been loaded',
+    },
+    errors: {
+      urlInvalid: 'Must be a valid URL',
+      rssInvalid: 'Source contains invalid RSS',
+      rssDuplicated: 'RSS already exists',
+      networkError: 'Network error',
+    },
+    elements: {
+      title: 'RSS aggregator',
+      subTitle: "Start reading RSS today! It's easy, it's beautiful.",
+      rssLink: 'RSS link',
+      mainButton: 'Add',
+      exampleUrl: 'Example: https://ru.hexlet.io/lessons.rss',
+      feeds: 'Feeds',
+      posts: 'Posts',
+      postButton: 'Watch',
+    },
+    modal: {
+      linkButton: 'Read completely',
+      closeButton: 'Close',
+    },
+  },
+};
