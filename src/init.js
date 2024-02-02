7207aba98a8373dff6a08e01785d8a6f60990a15
Sam <burtman.samuel26@gmail.com>
2024-02-02 21:49:25 +0200

    update src/init.js

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import 'bootstrap';
 import i18next from 'i18next';
 import * as yup from 'yup';
 import view from './view.js';
@@ -19,7 +20,9 @@ export default () => {
       clickedPostId: '',
     },
   };
+
   const delayTime = 5000;
+
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('#url-input'),
@@ -41,13 +44,16 @@ export default () => {
       modalCloseButton: document.querySelector('.modal-footer').querySelector('button'),
     },
   };
+
   const i18n = i18next.createInstance();
   const defaultLanguage = 'ru';
+
   i18n.init({
     lng: defaultLanguage,
     debug: false,
     resources,
   });
+
   yup.setLocale({
     string: {
       url: i18n.t('errors.urlInvalid'),
@@ -56,7 +62,9 @@ export default () => {
       notOneOf: i18n.t('errors.rssDuplicated'),
     },
   });
+
   const watchedState = view(state, elements, i18n);
+
   window.addEventListener('DOMContentLoaded', () => {
     const {
       title,
@@ -65,49 +73,65 @@ export default () => {
       exampleUrl,
       mainButton,
     } = elements.selectorsToTranslate;
+
     title.textContent = i18n.t('elements.title');
     subTitle.textContent = i18n.t('elements.subTitle');
     rssLink.textContent = i18n.t('elements.rssLink');
     exampleUrl.textContent = i18n.t('elements.exampleUrl');
     mainButton.textContent = i18n.t('elements.mainButton');
   });
+
   elements.form.addEventListener('submit', (event) => {
     event.preventDefault();
+
     const formData = new FormData(event.target);
     const currentUrl = formData.get('url').trim();
+
     const schema = yup.string()
       .required()
       .url(i18n.t('errors.urlInvalid'))
       .notOneOf(watchedState.form.urls, i18n.t('errors.rssDuplicated'));
+
     schema.validate(currentUrl)
       .then(() => {
         watchedState.form.valid = true;
         watchedState.form.processState = 'dataSending';
+
         return loadRss(currentUrl);
       })
       .then((response) => {
         watchedState.form.processState = 'dataSent';
         const { feed, posts } = response;
+
         watchedState.feeds = [...watchedState.feeds, feed];
         watchedState.posts = [...watchedState.posts, ...posts];
+
         watchedState.form.processState = 'rssLoaded';
         watchedState.form.valid = true;
         watchedState.form.urls.push(currentUrl);
+
         return response;
       })
       .catch((error) => {
         watchedState.form.error = error.type;
       });
   });
+
   elements.posts.addEventListener('click', (evt) => {
     const { target } = evt;
+
     if (target.nodeName === 'BUTTON') {
+      watchedState.modal.clickedPost = target;
       watchedState.modal.clickedPostId = target.dataset.id;
     }
+
     if (target.nodeName === 'A') {
-      target.classList.remove('fw-bold');
-      target.classList.add('fw-normal', 'link-secondary');
+      target.classList.replace('fw-bold', 'fw-normal');
+      target.classList.add('link-secondary');
     }
   });
+
   setTimeout(() => reloadSource(watchedState), delayTime);
 };
+
+// http://lorem-rss.herokuapp.com/feed?unit=second&interval=5&length=1 // => generate 1 feed every 5 sec
