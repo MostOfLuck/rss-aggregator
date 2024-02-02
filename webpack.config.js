0a71fc8e07a17cd0feb0b75e691a0d7e26e1440e
Sam <burtman.samuel26@gmail.com>
2024-02-02 14:18:48 +0200

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,15 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-import path, { dirname } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { fileURLToPath } from 'url';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-
-const filename = fileURLToPath(import.meta.url);
-const dirName = dirname(filename);
+// Since __dirname is not available in ES module scope, we use a workaround for CommonJS
+const dirName = path.resolve(__dirname);
 
 const isProduction = process.env.NODE_ENV === 'production';
-
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(dirName, 'dist'),
+    path: path.resolve(dirName, 'public'),
   },
   devServer: {
     open: true,
@@ -28,7 +24,6 @@ const config = {
       chunkFilename: '[id].css',
     }),
     // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
@@ -41,12 +36,10 @@ const config = {
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
-
-export default () => {
+module.exports = () => {
   if (isProduction) {
     config.mode = 'production';
   } else {
