968a10f1604b7480dfb1a5e2297d2ff505ad8217
Sam <burtman.samuel26@gmail.com>
2024-02-05 22:58:27 +0200

    update src/init.js

--- src/init.js
+++ src/init.js
@@ -1,9 +1,11 @@
 import 'bootstrap';
+import axios from 'axios';
+import { uniqueId } from 'lodash';
 import i18next from 'i18next';
 import * as yup from 'yup';
+import parse from './parse.js';
 import view from './view.js';
 import resources from '../languages/index.js';
-import { loadRss, reloadSource } from './parse.js';
 
 export const makeProxyURL = (url) => {
   const newProxyURL = new URL('https://allorigins.hexlet.app');
@@ -15,6 +17,63 @@ export const makeProxyURL = (url) => {
   const resultingURL = newProxyURL.href.toString();
 
   return resultingURL;
+};
+
+export const loadRss = async (url) => {
+  const proxy = makeProxyURL(url);
+
+  try {
+    const response = await axios.get(proxy);
+    const content = response.data.contents;
+    const parsedContent = parse(content);
+    const { feed, posts } = parsedContent;
+
+    feed.id = uniqueId();
+
+    posts.forEach((post) => {
+      post.id = uniqueId();
+      post.feedId = feed.id;
+    });
+
+    return parsedContent;
+  } catch (error) {
+    if (error.name === 'AxiosError') {
+      const networkError = new Error();
+      networkError.type = 'networkError';
+      throw networkError;
+    }
+
+    if (error.name === 'parsingError') {
+      const parsingError = new Error();
+      parsingError.type = 'parsingError';
+      throw parsingError;
+    }
+
+    return error.message;
+  }
+};
+
+const updatePosts = (response, posts) => {
+  const newPosts = response.posts;
+  const loadedPostsTitles = [];
+
+  posts.forEach((post) => loadedPostsTitles.push(post.title));
+  const diffPosts = newPosts.filter((post) => !loadedPostsTitles.includes(post.title));
+
+  if (diffPosts.length !== 0) {
+    diffPosts.forEach((diffPost) => posts.push(diffPost));
+  }
+};
+
+export const reloadSource = (watchedState) => {
+  const { posts } = watchedState;
+  const { urls } = watchedState.form;
+
+  const requests = urls.map((item) => loadRss(item));
+
+  Promise.all(requests)
+    .then((responses) => responses.forEach((response) => updatePosts(response, posts)))
+    .finally((setTimeout(() => reloadSource(watchedState), 5000)));
 };
 
 export default () => {
